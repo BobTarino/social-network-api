73b98a9797f8d47b42c262d4bf335bab5622441e
Bobby T <bob.tarino@gmail.com>
2021-07-23 13:44:01 -0500

    created user-controllerwith CRUD methods

---
+++ controllers/user-controller.js
@@ -0,0 +1,106 @@
+const { User } = require('../models');
+
+const UserController = {
+    // get all Users (GET /api/Users)
+    getAllUser(req, res) {
+      User.find({})
+      // populates field with stored thought and friend associated to User
+      .populate([
+        { path: 'thoughts', select: "-__v" },
+        { path: 'friends', select: "-__v" }
+      ])
+      .select('-__v')
+      .sort({ _id: -1 })
+      .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+          console.log(err);
+          res.status(400).json(err);
+        });
+    },
+  
+    // get one User by id
+    getUserById({ params }, res) {
+      User.findOne({ _id: params.id })
+      .populate([
+        { path: 'thoughts', select: "-__v" },
+        { path: 'friends', select: "-__v" }
+        ])
+        .select('-__v')
+        // do not need ".sort()" method here because we'd only be sorting a single User
+        .then(dbUserData => {
+          if (!dbUserData) {
+            res.status(404).json({ message: 'No User found with this id!' });
+            return;
+          }
+          res.json(dbUserData);
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(400).json(err);
+        });
+    },
+  
+    // create User (POST /api/users)
+    createUser({ body }, res) {
+      User.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.status(400).json(err));
+    },
+  
+    // update User by id (PUT /api/users/:userId)
+    updateUser({ params, body }, res) {
+      // {new: true} instructs mongoose to return new version of document with Validators 
+      User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+        .then(dbUserData => {
+          if (!dbUserData) {
+            res.status(404).json({ message: 'No User found with this id!' });
+            return;
+          }
+          res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+  
+    // delete User (DELETE /api/users/:userId)
+    deleteUser({ params }, res) {
+      User.findOneAndDelete({ _id: params.id })
+        .then(dbUserData => {
+          if (!dbUserData) {
+            res.status(404).json({ message: 'No User found with this id!' });
+            return;
+          }
+          res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+    // add friend (POST /api/users/:userId/friends/:friendId)
+    addFriend({ params }, res) {
+        // add friendId to userId's friend list
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $push: { friends: params.friendId } },
+            { new: true, runValidators: true }
+        )
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this userId' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
+    },
+
+    // DELETE /api/users/:userId/friends/:friendId
+    deleteFriend( { params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $pull: { friends: params.friendId }},
+            { new: true}
+        )
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.json(err));
+    }
+}
+  
+module.exports = UserController;
--- models/User.js
+++ models/User.js
@@ -28,7 +28,7 @@ const UserSchema = new Schema(
             type: Schema.Types.ObjectId,
             ref: 'User'
         }
-    ]
+      ]
     },
     {
       toJSON: {
