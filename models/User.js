768cc6ff007950b5f8e80b7739b70d1ac2ab7c67
Bobby T <bob.tarino@gmail.com>
2021-07-22 22:08:14 -0500

    installed mongoose, created User model & friends virtual

---
+++ models/User.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require('mongoose');
+
+// Schema 
+const UserSchema = new Schema(
+    {
+      username: {
+        type: String,
+        required: 'You need to provide a pizza name!',
+        unique: true,
+        trim: true
+      },
+      email: {
+        type: String,
+        required: 'You need to provide a username!',
+        unique: true,
+        trim: true,
+        match: [/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 'Please fill a valid email address']
+      },
+      thoughts: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Thought'
+        }
+      ],
+      friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+    },
+    {
+      toJSON: {
+        virtuals: true,
+        getters: true
+      },
+      id: false
+    }
+);
+
+
+// create the User model using the UserSchema
+const User = model('User', UserSchema);
+
+// get total count of friends onretrieval
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length 
+});
+
+// export the User model
+module.exports = User;
